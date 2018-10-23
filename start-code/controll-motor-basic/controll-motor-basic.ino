86dda54fc6da3767414502fc7c61cfd877e4295d
datduyng <datnguyen5653@huskers.unl.edu>
2018-10-22 21:42:55 -0500

    update i2c arduino rasp communication starter code

--- start-code/controll-motor-basic/controll-motor-basic.ino
+++ start-code/controll-motor-basic/controll-motor-basic.ino
@@ -1,7 +1,7 @@
 //Wiring Connections
 #define ENA 10  // Pin #10 or Any pin that supports PWM output, for variable speed control
-#define DR1 9   // Pin #10 or Any pin with digital output, for directional control
-#define DR2 8   // Pin #10 or Any pin with digital output, for directional control
+#define DR1 9   // Pin #9 or Any pin with digital output, for directional control
+#define DR2 8   // Pin #8 or Any pin with digital output, for directional control
  
 void setup(){
   // All motor control pins are outputs
@@ -10,7 +10,7 @@ void setup(){
   pinMode(DR2, OUTPUT);
 }
 
-void loop(){
+void runMotorCW(){
   // Set the motor to run in one direction, this depends on your wiring
   digitalWrite(DR1, HIGH);
   digitalWrite(DR2, LOW);
@@ -21,13 +21,11 @@ void loop(){
     // Adding a bit of delay
     delay(2);
   }
-  
-  delay(100);
-  // Stop the motor
-  analogWrite(ENA, 0);
-  
+}
+
+void runMotorCCW(){// turn motor the otherway. 
   digitalWrite(DR1, LOW);
-  digitalWRite(DR2, HIGH);
+  digitalWrite(DR2, HIGH);
 
   for( int i = 0; i < 256 ; i++ ){
     // Slowly ramp up the speed
@@ -37,8 +35,17 @@ void loop(){
   }
   
   delay(100);
+}
+
+void stopMotor(){
   // Stop the motor
   analogWrite(ENA, 0);
-  
+}
 
+void loop(){
+  runMotorCW();
+  delay(1000);
+  runMotorCCW();
+  delay(1000);
+  stopMotor();
 }
--- start-code/i2c-master-slave-rasp-demo/slave1-arduino-motor/slave1-arduino-motor.ino
+++ start-code/i2c-master-slave-rasp-demo/slave1-arduino-motor/slave1-arduino-motor.ino
@@ -4,15 +4,32 @@
 //https://www.youtube.com/watch?v=Jndb2vpAWwU
 
 #include<Wire.h>
-#define SLAVE2_ADD 0x04
+
+//Wiring Connections for motor
+#define ENA 10  // Pin #10 or Any pin that supports PWM output, for variable speed control
+#define DR1 9   // Pin #9 or Any pin with digital output, for directional control
+#define DR2 8   // Pin #8 or Any pin with digital output, for directional control
+
+#define SLAVE1_ADD 0x04
+
+/**FUNCTION prototype**/
+void runMotorCW();
+void stopMotor();
 
 void setup(){
 
-  Wire.begin(SLAVE2_ADD);// device with address SLAVE2_ADD
+  Wire.begin(SLAVE1_ADD);// device with address SLAVE2_ADD
+  
   Wire.onReceive(receiveEvent);
   Serial.begin(9600);
-  pinMode(LED_BUILTIN,OUTPUT);
- // digitalWrite(LED_BUILTIN,LOW);
+
+  //config motor
+  // All motor control pins are outputs
+  pinMode(ENA, OUTPUT);
+  pinMode(DR1, OUTPUT);
+  pinMode(DR2, OUTPUT);
+
+  
 }
 
 void loop(){
@@ -26,13 +43,33 @@ void receiveEvent(int howMany){
     Serial.print("c ; ");Serial.println(c);
     if(c == '1'){
       Serial.println("H");
-      digitalWrite(LED_BUILTIN,HIGH);
+        runMotorCW();
+       
     }
     else if( c == '0'){
-      digitalWrite(LED_BUILTIN,LOW);
-      Serial.println("L");
+      stopMotor();
     }else{
       Serial.println("Invalid command");
     }
   }
+}
+
+void runMotorCW(){
+  // Set the motor to run in one direction, this depends on your wiring
+  digitalWrite(DR1, HIGH);
+  digitalWrite(DR2, LOW);
+
+  for( int i = 0; i < 256 ; i++ ){
+    // Slowly ramp up the speed
+    analogWrite(ENA, i);
+    // Adding a bit of delay
+    delay(2);
+  }
+}
+
+void stopMotor(){
+  // Stop the motor
+  digitalWrite(DR1, LOW);
+  digitalWrite(DR2, LOW);
+  analogWrite(ENA, 0);
 }
